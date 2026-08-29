c0a0f73d3c30789c129970918bf757ad28174d5d
Alexander Moiseykin <yousero.art@gmail.com>
2026-08-29 12:06:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// const greens = document.querySelectorAll('.section')
+
+// for (const g of greens) {
+//   g.addEventListener('click', (e) => {
+//     e.target.style = 'display: none;'
+//   })
+// }
